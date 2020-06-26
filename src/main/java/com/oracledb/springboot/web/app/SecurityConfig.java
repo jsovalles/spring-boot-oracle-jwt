79620eff3595304ac56eb63c04fb2881cb82787b
jsovalles <jsovalles95@gmail.com>
2020-06-26 11:35:04 -0500

    auth0 example

--- src/main/java/com/oracledb/springboot/web/app/SecurityConfig.java
+++ src/main/java/com/oracledb/springboot/web/app/SecurityConfig.java
@@ -1,34 +1,36 @@
 package com.oracledb.springboot.web.app;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
+import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
 import com.oracledb.springboot.web.app.auth.IJWTService;
 import com.oracledb.springboot.web.app.auth.filter.JWTAuthenticationFilter;
 import com.oracledb.springboot.web.app.auth.filter.JWTAuthorizationFilter;
 
 
-@EnableGlobalMethodSecurity(securedEnabled=true, prePostEnabled=true)
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value(value = "${auth0.apiAudience}")
+    private String apiAudience;
+    @Value(value = "${auth0.issuer}")
+    private String issuer;
 
-	@Autowired
-	private IJWTService jwtService;
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-
-		http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**", "/customers/v1/").permitAll()
-		.anyRequest().authenticated()
-		.and()
-		.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtService))
-		.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtService))
-		.csrf().disable()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-	}
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        JwtWebSecurityConfigurer
+                .forRS256(apiAudience, issuer)
+                .configure(http)
+                .cors().and().csrf().disable().authorizeRequests()
+                .anyRequest().authenticated();
+        
+    }
 }
